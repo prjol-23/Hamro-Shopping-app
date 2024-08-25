422ba38a9591f5cc081b67f04b591edae4d57c3d
Prajwol Bista <prajwolbista144@gmail.com>
2024-08-25 08:58:18 +0545

    Deploy github pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Hamro-Shopping-App/', // Replace 'repository-name' with your actual GitHub repo name
-});
